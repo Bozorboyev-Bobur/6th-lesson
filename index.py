4ecc06ebb01efcfbf312417c3c26ad0bcfe56622
Bobur Bazarbayev <bozorboyevbobur2004@gmail.com>
2021-11-16 23:54:50 +0500

    first commit

---
+++ index.py
@@ -0,0 +1,41 @@
+# age = int(input('Enter your age: '))
+
+# if 18 <= age <=50: print('Welcome!')
+# else: print('Bye!')
+
+# num = int(input('Enter your number: '))
+# print(num // 1 % 10)
+# print(num // 10 % 10)
+# print(num // 100 % 10)
+# print(num // 1000 % 10)
+# print(num // 10000 % 10)
+
+# cost = int(input('Введите сумму:'))
+# money = int(input('Сумма:'))
+# if cost % 100 == 0 and money % 100 == 0:
+#     change=money - cost
+# five_hundred = change // 500
+# change = change % 500
+
+# two_hundred = change // 200
+# change = change % 200
+
+# hundred = change // 100
+# print(five_hundred , two_hundred, hundred)
+
+# num = float(input('Enter 1st number: '))
+# num2 = float(input('Enter 2nd number: '))
+# num3 = float(input('Ender 3rd number: '))
+
+# if num > num2 > num3: print(num , num2 , num3)
+# if num > num3 > num2: print(num , num3 , num2)
+# if num2 > num > num3: print(num2 , num , num3)
+# if num2 > num3 > num: print(num2 , num3 , num)
+# if num3 > num2 > num: print(num3 , num2 , num)
+# if num3 > num > num2: print(num3 , num , num2)
+
+# large = max(num, num2, num3)
+# small = min(num, num2, num3)
+# middle = num + num2 + num3 - large - small 
+# print(large, middle, small)
+
